1b1e3c4dba8ac3e36cfb2625dd6282a02c6465b9
nguyenloc2001 <locsoi2001@gmail.com>
2023-12-14 16:50:25 +0700

    Common Connect DataBase

--- QuanLySanXuat/Site.Master.cs
+++ QuanLySanXuat/Site.Master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLySanXuat.ConnectDataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace QuanLySanXuat
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string queryString = @"SELECT * FROM LenhSanXuat";
+            var data = ConnectData.Execute_NonSQL(queryString);
         }
+
     }
 }
